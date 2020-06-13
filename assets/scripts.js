e6e059a381484059773400a03f5f736a5553e50a
abtanjir <abtanjir@gmail.com>
2020-06-14 02:27:04 +0600

    passing nonce & UI Improvement

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,7 +2,7 @@ jQuery(function($){
     "use strict"
     
     var submit_url = $('#filebuddy_uploader').data('upload');
-    // console.log(200 * 1024 * 1024);
+    
     $('.file_upload').uploader({
         upload_url: submit_url,
         input_name: 'file',
@@ -10,26 +10,39 @@ jQuery(function($){
         maximum_total_files: 4,
         maximum_file_size: 200 * 1024 * 1024,
         minimum_file_size: 20 * 1024 * 1024,
+        nonce: uploader.nonce,
         file_types_allowed: ['video/mp4', 'video/3gp', 'video/ogg', 'video/mov'],
-        on_before_upload: function(e) {
-            $(e).closest('form').find(":submit").prop('disabled', true);
-            // $('.save-manual').attr('disabled', true)
+        on_before_upload: function(el) {
+            $(el).prop('disabled', true);
+            var status_dom = $(el).closest('div').find('.uploader_status');
+            if(status_dom.length){
+                $(status_dom).text('Processing...');
+            }else{
+                $(el).closest('div').append('<p class="uploader_status">Processing...</>')
+            }
         },
-        on_success_upload: function(e) {
-            // $(e).closest('form').find(":submit").prop('disabled', false);
-            // $('.save-manual').attr('disabled', false)
+        on_success_upload: function(el) {
+            $(el).prop('disabled', false);
+            var status_dom = $(el).closest('div').find('.uploader_status');
+            if(status_dom.length){
+                $(status_dom).text('Uploaded');
+            }else{
+                $(el).closest('div').append('<p class="uploader_status">Uploaded</>')
+            }
         },
         on_finish_upload: function(e) {
-            $(e).closest('form').find(":submit").prop('disabled', false);
+
         },
         on_error: function(err, el) {
             var message;
+            var err_dom = $(el).closest('div').find('.uploader-error');
+
             if(err.status == "error_maximum_file_size"){
                 var message = 'File is too large';
             }else if(err.status == "error_minimum_file_size"){
                 var message = 'File is too small';
             }
-            var err_dom = $(el).closest('div').find('.uploader-error');
+
             if($(err_dom).length){
                 $(err_dom).html(message);
             }else{
@@ -38,4 +51,9 @@ jQuery(function($){
 
         }
     })
+
+
+
+
+
 });
